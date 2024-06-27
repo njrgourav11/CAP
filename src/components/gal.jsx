ac290d1a4b51e0d79dab8b02382f23414a701ed8
njrgourav11 <njrgourav@gmail.com>
2024-06-27 16:13:13 +0530

    Update gal.jsx

--- src/components/gal.jsx
+++ src/components/gal.jsx
@@ -1,20 +1,31 @@
+import React from 'react';
+import img1 from '../assets/gal/1.jpg';
+import img2 from '../assets/gal/2.jpg';
+import img3 from '../assets/gal/3.jpg';
+import img4 from '../assets/gal/4.jpg';
+import img5 from '../assets/gal/5.jpg';
+import img6 from '../assets/gal/6.jpg';
+import img7 from '../assets/gal/7.jpg';
+import img8 from '../assets/gal/8.jpg';
+import img9 from '../assets/gal/9.jpg';
+
 const Gallery1 = () => {
-    // Generate an array of image file names from 1.jpg to 9.jpg
-    const imageNames = Array.from({ length: 9 }, (_, index) => `${index + 1}.jpg`);
+  // Array of image imports
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
 
-    return (
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 ml-8 mr-8">
+      {images.map((image, index) => (
+        <div key={index} className="w-full">
+          <img
+            className="object-cover w-full h-64 rounded-lg"
+            src={image}
+            alt={`Image ${index + 1}`}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 ml-8 mr-8">
-        {imageNames.map((imageName, index) => (
-          <div key={index} className="w-full">
-            <img
-              className="object-cover w-full h-64 rounded-lg"
-              src={`src/assets/gal/${imageName}`} 
-              alt={`Image ${index + 1}`}
-            />
-          </div>
-        ))}
-      </div> 
-      );
-      };
-      export default Gallery1;
+export default Gallery1;
