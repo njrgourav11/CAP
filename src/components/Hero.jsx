f4ae1b17637c3c9cdc2988d1804e10f7aa265fff
njrgourav11 <njrgourav@gmail.com>
2024-07-03 14:50:50 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import styles from "../style";
-import { discount, cap } from "../assets";
+import { discount, cap, telangana } from "../assets";
 import GetStarted from "./GetStarted";
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <img src={telangana} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">Women</span> Safety Wing,{" "}
             <span className="text-white">Telangana</span> Police &{" "}
